86bfbb10276ed086acb2ca77249bc6751c422ed1
Seongsoo Yoon <cieltero@gmail.com>
2017-02-11 23:40:28 +0900

    ignore config.php, add config.template.php

---
+++ config/config.template.php
@@ -0,0 +1,14 @@
+<?php
+$config=array(
+    "host" => "",
+    "duser" => "l",
+    "dpw" => "",
+    "dname" => ""
+);
+$config2 = array(
+    "host" => "localhost",   
+    "duser" => "",
+    "dpw" => "",
+    "dname" => ""
+);
+?>
